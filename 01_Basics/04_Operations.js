e23f826755adba5c8ab349e81282cb86871c6dd5
AditapKumar <115876365+AditapKumar@users.noreply.github.com>
2024-08-26 14:41:02 +0000

    Some basic operators

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,47 @@
+// let value = 3;
+// let negativevalue = -value
+// console.log(negativevalue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(3/2)
+// console.log(4%2)
+
+// let str1 = "Addy"
+// let str2 = " Rawal"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1"+2);     //12
+// console.log(1+"2");     //12
+// console.log("1"+"2");   //12
+// console.log("1"+2+2);   //122
+// console.log(1+2+"2");   //32
+
+// console.log((3+4)*5%3);
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3  //Bad Practice of assigning values
+// num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100;
+// gameCounter++
+// console.log(gameCounter);
+
+//prefix inc
+// let x = 5
+// let y = ++x
+// console.log(x); //6
+// console.log(y); //6
+
+//postfix inc
+// let x = 5
+// let y = x++
+// console.log(x); //6
+// console.log(y); //5
+
