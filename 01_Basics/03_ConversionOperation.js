4234f1ec1615b6b59881e9ebe5b18faee4f9972a
AditapKumar <115876365+AditapKumar@users.noreply.github.com>
2024-08-26 13:48:34 +0000

    Conversion of Datatypes

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,41 @@
+//Number
+// let score = 33;
+// console.log(typeof score)
+// console.log(typeof (score))
+
+//String & String containing alphabets
+// let score2 = "33abc"
+// console.log(typeof score2);
+// let value = Number(score2)
+// console.log(value);
+// console.log(typeof value);
+
+//Null value
+// let score3 = null
+// console.log(typeof score3);
+// let value2 = Number(score3)
+// console.log(value2);
+// console.log(typeof value2);
+
+//Undefined
+// let score4 = undefined
+// console.log(typeof score4);
+// let value3 = Number(score3)
+// console.log(value3);
+// console.log(typeof value3);
+
+//boolean values
+// let isLoggedIn = 1
+// let isLoggedIn = "" 
+// let isLoggedIn = "Adxrxwxl" 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+// 1 - true, 0 - false
+// "" - false
+// "abc" - true
+
+let somenumber = 33
+let stringofnumber = String(somenumber)
+console.log(stringofnumber);   //"33"
+console.log(typeof stringofnumber);  //string
