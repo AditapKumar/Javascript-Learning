37a657316f1996d29ce7ec86909cb87f7bb9a674
AditapKumar <115876365+AditapKumar@users.noreply.github.com>
2024-08-27 12:18:36 +0000

    Comparision of Datatypes using relational operators

---
+++ 01_Basics/05_ComparisonOfDatatypes.js
@@ -0,0 +1,21 @@
+//Basic comparison
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+
+//Comp. with different datatypes
+// console.log("2" > 1);    //True
+// console.log("02" > 1);   //True 
+
+// In the Comparison (1) & (3), the null is converted into 0, whereas equality one works in different way.
+// console.log(null > 0);   // false
+// console.log(null == 0);  //false
+// console.log(null >= 0);  //true
+
+// console.log(undefined == 0);     //All 3 false
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+//strict check (===)
+console.log("2" == 2);
+console.log("2" === 2);
