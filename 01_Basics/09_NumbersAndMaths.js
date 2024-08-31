c78a7dd94d4f66b77c112955a07076e6c2574b6d
AditapKumar <115876365+AditapKumar@users.noreply.github.com>
2024-08-31 16:04:46 +0000

    Basic numbers & maths

--- 01_Basics/09_NumbersAndMaths.js
+++ 01_Basics/09_NumbersAndMaths.js
@@ -18,3 +18,24 @@ console.log(otherNumber.toPrecision(3))
 const hundreds = 1000000
 console.log(hundreds.toLocaleString())    //U.S. Standards
 console.log(hundreds.toLocaleString('en-IN'))       //Indian Standards
+
+// +++++++++++++ Maths ++++++++++++++++++++
+
+// console.log(Math);
+
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.3))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.99))
+// console.log(Math.min(4, 5, 7, 1, 9, 2))
+// console.log(Math.max(4, 5, 7, 1, 9, 2))
+
+console.log(Math.random());  //Random values b/w 0 to 1
+console.log((Math.random()*10) + 1); // >1
+console.log(Math.floor(Math.random()*10) + 1); //Floor round-off value > 1
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) // value>10 & floor-roundoff
+// floor(0.93 * (20-10+1)) + 10
